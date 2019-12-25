312b9621f21dade5f543f12d3dcdd338643fc5a7
xx132917 <32615513+xx132917@users.noreply.github.com>
2019-12-25 10:53:30 +0800

    操作etcd

--- interview/etcd_practice/etcd.go
+++ interview/etcd_practice/etcd.go
@@ -3,40 +3,73 @@ package main
 import (
 	"context"
 	"fmt"
-	"go.etcd.io/etcd/client"
+	"go.etcd.io/etcd/clientv3"
+	"log"
 	"time"
 )
 
-func main() {
-	//链接etcd
-	cfg := client.Config{
-		Endpoints: []string{"http://192.168.10.36:32799/", "http://192.168.10.36:32801/", "http://192.168.10.36:32803/"},
-		Transport: client.DefaultTransport,
-		//set timeout per request to fail fast when the target endpoint is unavailable
-		HeaderTimeoutPerRequest: time.Second,
+func conn() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"192.168.10.36:2379", "192.168.10.36:2380"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Println(err.Error())
 	}
-	c, _ := client.New(cfg)
-	keysApi := client.NewKeysAPI(c)
+	defer cli.Close()
+	fmt.Println("conn success")
 
-	//读取etcd
-	res, err := keysApi.Get(context.Background(), "job", nil)
-	fmt.Println("res====>", res)
-	fmt.Println("err====>", err)
-	//添加etcd
-	res1, err := keysApi.Set(context.Background(), "work", "This is my work", nil)
-	fmt.Println("res1===>", res1)
-	fmt.Println("err===>", err)
-	//读取etcd
-	values, err := keysApi.Get(context.Background(), "work", nil)
-	fmt.Println("values====>", values)
-	fmt.Println("err====>", err)
-	//更新etcd
-	up, err := keysApi.Update(context.Background(), "job", "my job")
-	fmt.Println("up====>", up)
-	fmt.Println("err====>", err)
-	//删除
-	del, err := keysApi.Delete(context.Background(), "work", nil)
-	fmt.Println("del====>", del)
-	fmt.Println("err====>", err)
+	//设置超时及设置值
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 50*time.Second)
+	_, err = cli.Put(ctx, "name", "Jaye")
+	cancelFunc()
+	if err != nil {
+		log.Println("cli.Put", err.Error())
+	}
 
+	//取值
+	ctx, cancelFunc = context.WithTimeout(context.Background(), 50*time.Second)
+	res, err := cli.Get(ctx, "name")
+	if err != nil {
+		log.Println("cli.Get", err.Error())
+	}
+
+	for k, v := range res.Kvs {
+		fmt.Println("查询结果", k, string(v.Key), string(v.Value))
+	}
+}
+
+func watch() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"192.168.10.36:2379", "192.168.10.36:2380"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer cli.Close()
+	fmt.Println("conn success")
+
+	//设置超时及设置值
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
+	_, err = cli.Put(ctx, "home", "sdfwsdf ")
+	cancelFunc()
+	if err != nil {
+		log.Println("cli.Put", err.Error())
+	}
+
+	//watch
+	for {
+		rch := cli.Watch(context.Background(), "home")
+		for resp := range rch {
+			for k, v := range resp.Events {
+				fmt.Println(k, v.Type, string(v.Kv.Key), string(v.Kv.Value))
+			}
+		}
+	}
+}
+
+func main() {
+	conn()
+	watch()
 }
